71007b3a4bed9fcc284f7e341ca73357fe6589fc
Dmitry M <dmitrym@gmail.com>
2021-03-09 09:56:50 -0800

    Initial Commit

---
+++ dropbox-test/dropbox_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  dropbox_testApp.swift
+//  dropbox-test
+//
+//  Created by Dmitry Markushevich on 2021-03-09.
+//
+
+import SwiftUI
+
+@main
+struct dropbox_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
